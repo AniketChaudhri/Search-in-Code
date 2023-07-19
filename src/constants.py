8b2cb612da80461ecd75b0ff1fb62930b17da917
Aniket Chaudhri <aniket.chaudhri@oppo.com>
2023-07-19 15:25:19 +0530

    Add function extractor in Embeddings class

---
+++ src/constants.py
@@ -0,0 +1,17 @@
+EXTENSIONS = {
+    '.rb': 'ruby',
+    '.go': 'go',
+    '.rs': 'rust',
+    '.java': 'java',
+    '.js': 'javascript',
+    '.ts': 'typescript',
+    '.py': 'python',
+    '.c': 'c',
+    '.h': 'c',
+    '.cpp': 'cpp',
+    '.hpp': 'cpp',
+    '.kt': 'kotlin',
+    '.kts': 'kotlin',
+    '.ktm': 'kotlin',
+    '.php': 'php',
+}
---
+++ src/embeddings.py
@@ -0,0 +1,48 @@
+import tqdm
+import os
+
+from tree_sitter import Tree
+from tree_sitter_languages import get_parser
+
+from constants import EXTENSIONS
+
+class Embeddings:
+    def __init__(self):
+        pass
+
+    def _traverse_tree(tree: Tree):
+        cursor = tree.walk()
+        reached_root = False
+        while reached_root is False:
+            yield cursor.node
+            if cursor.goto_first_child():
+                continue
+            if cursor.goto_next_sibling():
+                continue
+            retracing = True
+            while retracing:
+                if not cursor.goto_parent():
+                    retracing = False
+                    reached_root = True
+                if cursor.goto_next_sibling():
+                    retracing = False
+
+    def _get_functions(self, repo_path, extract_nodes):
+        functions = []
+        print('Extracting functions from {}'.format(repo_path))
+        for fp in tqdm([repo_path + '/' + f for f in os.popen('git -C {} ls-files'.format(repo_path)).read().split('\n')]):
+            if not os.path.isfile(fp):
+                continue
+            with open(fp, 'r') as f:
+                lang = EXTENSIONS.get(fp[fp.rfind('.'):])
+                if lang:
+                    parser = get_parser(lang)
+                    try:
+                        file_content = f.read()
+                    except UnicodeDecodeError:
+                        file_content = ''
+                    tree = parser.parse(bytes(file_content, 'utf8'))
+                    all_nodes = list(self._traverse_tree(tree.root_node))
+                    functions.extend(self._extract_functions(
+                        all_nodes, fp, file_content, extract_nodes))
+        return functions
