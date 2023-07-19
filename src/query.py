10a49db0f1da12357f35d787dbc8ad77fa9aceaf
Aniket Chaudhri <aniket.chaudhri@oppo.com>
2023-07-19 12:10:14 +0530

    Add query.py

---
+++ src/query.py
@@ -0,0 +1,31 @@
+from loguru import logger
+import gzip
+import pickle
+
+class Query:
+    def __init__(self):
+        pass
+
+    def _query_embeddings(self, model, args):
+        if not self._embeddings_exists(args):
+            # call function to create embeddings
+            # create_embeddings(model, args)
+            pass
+
+        with gzip.open(args.path_to_repo + '/' + '.embeddings', 'r') as f:
+            dataset = pickle.loads(f.read())
+            # TODO: check if current embeddings model is same as provided
+            query_embedding = model.encode(args.query_text, convert_to_tensor=True)
+            result = self._find_closest_embeddings(query_embedding, dataset)
+            return result
+
+    def perform_query(self, model, args):
+        if not args.query_text:
+            logger.info("No query text provided")
+            return None
+
+        result = self._query_embeddings(model, args)
+
+        return result
+
+
