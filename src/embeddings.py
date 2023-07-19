a935cdb6e4c51a68cfd490b647c8bd2ab4d0b248
Aniket Chaudhri <aniket.chaudhri@oppo.com>
2023-07-19 16:06:27 +0530

    Update Embeddings and Add unit_tester

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,17 +1,23 @@
-import tqdm
+import gzip
+import pickle
+import unittest
+import numpy as np
+from tqdm import tqdm
 import os
+import sys
 
 from tree_sitter import Tree
 from tree_sitter_languages import get_parser
 
 from constants import EXTENSIONS
 from textwrap import dedent
+from sentence_transformers import SentenceTransformer
 
 class Embeddings:
     def __init__(self):
         pass
 
-    def _traverse_tree(tree: Tree):
+    def _traverse_tree(self, tree: Tree):
         cursor = tree.walk()
         reached_root = False
         while reached_root is False:
@@ -28,7 +34,7 @@ class Embeddings:
                 if cursor.goto_next_sibling():
                     retracing = False
 
-    def _extract_functions(nodes, fp, file_content, relevant_node_types):
+    def _extract_functions(self, nodes, fp, file_content, relevant_node_types):
         out = []
         for n in nodes:
             if n.type in relevant_node_types:
@@ -57,3 +63,56 @@ class Embeddings:
                     functions.extend(self._extract_functions(
                         all_nodes, fp, file_content, extract_nodes))
         return functions
+
+    def embed(self, args, model):
+        nodes_to_extract = ['function_definition', 'method_definition',
+                        'function_declaration', 'method_declaration']
+        functions = self._get_functions(
+            args.path_to_repo, nodes_to_extract)
+
+        if not functions:
+            print('No supported languages found in {}. Exiting'.format(args.path_to_repo))
+            sys.exit(1)
+
+        print('Embedding {} functions in {} batches. This is done once and cached in .embeddings'.format(
+            len(functions), int(np.ceil(len(functions)/args.batch_size))))
+        corpus_embeddings = model.encode(
+            [f['text'] for f in functions], convert_to_tensor=True, show_progress_bar=True, batch_size=args.batch_size)
+
+        dataset = {'functions': functions,
+                'embeddings': corpus_embeddings, 'model_name': args.model_name_or_path}
+
+        # dump dataset
+        with open(args.path_to_repo + '/' + '.embeddings.log', 'w') as f:
+            f.write('dataset: {}\n'.format(dataset))
+
+        with gzip.open(args.path_to_repo + '/' + '.embeddings', 'w') as f:
+            f.write(pickle.dumps(dataset))
+        return dataset
+
+if __name__ == "__main__":
+    class TestEmbeddings(unittest.TestCase):
+
+        def testembed(self):
+            class args:
+                path_to_repo= r'C:\Users\S9053161\Documents\projects\Search-in-Code\src\gitrepos\78218'
+                model_name_or_path= 'krlvi/sentence-msmarco-bert-base-dot-v5-nlpl-code_search_net'
+                batch_size= 32
+
+            # args={
+            #     'path_to_repo': r'C:\Users\S9053161\Documents\projects\Search-in-Code\src\gitrepos\78218',
+            #     'model_name_or_path': 'krlvi/sentence-msmarco-bert-base-dot-v5-nlpl-code_search_net',
+            #     'batch_size': 32
+            # }
+            model = SentenceTransformer(args.model_name_or_path)
+            embeddings = Embeddings()
+            print(embeddings.embed(args, model))
+
+    RUN_ALL = False
+    if RUN_ALL:
+        unittest.main()
+    else:
+        suite = unittest.TestSuite()
+        suite.addTest(TestEmbeddings('testembed'))
+        runner = unittest.TextTestRunner()
+        runner.run(suite)
